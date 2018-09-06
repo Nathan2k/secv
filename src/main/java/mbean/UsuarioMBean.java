043675c1a1ff49da85dd4988bd8d5d7f98e35a08
Yamae <coelho.ycy@gmail.com>
2018-09-05 23:11:47 -0300

    MBean quase pronto
    Tai hugo! Pq n?

--- src/main/java/mbean/UsuarioMBean.java
+++ src/main/java/mbean/UsuarioMBean.java
@@ -1,5 +1,33 @@
 package mbean;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import DAO.UsuarioDAO;
+import entity.UsuarioADM;
+import entity.UsuarioEmpresa;
+
 public class UsuarioMBean {
 
+	UsuarioEmpresa uEmpresa;
+	UsuarioADM uAdm;
+	UsuarioDAO uDao;
+	
+	public String loginEmpresa() {
+		if(uDao.buscarUsuario(uEmpresa.getCNPJ())!= null) {
+			return "temfiltro.xgtml";
+		}
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Usuário e/ou  Senha invalido(s)"));
+		return null;
+	}
+	
+	
+//	public String loginAdm() {
+//		if(uDao.buscarUsuario())
+//	}
+	
+	
+//	value="#{UsuarioMBean.uEmpresa.CNPJ}"
+	
+//	action="#{usuarioMBean.doEfetuarLogin}"
 }
