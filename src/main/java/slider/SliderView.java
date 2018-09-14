e44e7d94d9e69c3300ccb8c7b04aff635e08ad21
Nathan Oliveira <nathan.oliveira1988@hotmail.com>
2018-09-14 11:05:48 -0300

    Modific. Telas
    Filtro
    Esqueci senha
    nova empresa

---
+++ src/main/java/slider/SliderView.java
@@ -0,0 +1,26 @@
+package slider;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class SliderView {
+
+	private int number8 = 18;
+	private int number9 = 40;
+	
+	
+	public int getNumber8() {
+		return number8;
+	}
+	public void setNumber8(int number8) {
+		this.number8 = number8;
+	}
+	public int getNumber9() {
+		return number9;
+	}
+	public void setNumber9(int number9) {
+		this.number9 = number9;
+	}
+	
+	
+}
