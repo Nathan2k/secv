36782a57edc89ce3359ebb8a06b89ffaf7aaad3e
Hugooo <hugosavioli@gmail.com>
2018-11-30 07:55:25 -0200

    Revert "Merge branch 'master' of https://github.com/Nathan2k/secv"
    This reverts commit 647ca4e4fabc5569723f9f5aaf5e4619767ae560, reversing
    changes made to f2d74602946d136ce9ba41db52087aa3871816ed.

---
+++ src/main/java/mbean/HistoricoMBean.java
@@ -0,0 +1,118 @@
+package mbean;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import DAO.CurriculoDAO;
+import entity.Curriculo;
+import entity.Filtro;
+import service.Experiencia;
+import service.Formacao;
+
+@ManagedBean
+@ViewScoped
+public class HistoricoMBean {
+
+	Curriculo cur;
+	Formacao ff;
+	Experiencia xp;
+
+	List<Curriculo> cl;
+	Filtro filtroSelecionado = new Filtro();
+
+	CurriculoDAO clDAO;
+
+	boolean filtroP = false; // é o rendered da tela depois de clicar pra vizualizar um filtro
+	boolean filtroF = true;
+
+	// VER SE ISSO TA CERTO
+	// A DIFERENÇA DE UM PRA OUTROÉ Q ESSE SE ENCONTRA EM OUTRA TELA E O XHTML JA
+	// ESTA ME TRAZENDO O NOME E O ID DO FILTRO ENTÃO É SO EU PEGAR O ID DO FILTRO E
+	// LISTAR
+	// ISSO PELO SETPROPERTYACTIONLISTENER
+	public void listarCur() {
+
+		cur = new Curriculo();
+		ff = new Formacao();
+		xp = new Experiencia();
+		clDAO = new CurriculoDAO();
+		cl = clDAO.listarCurriculo(filtroSelecionado.getId());
+		filtroP = true;
+		filtroF = false;
+		clDAO = new CurriculoDAO();
+
+	}
+	
+	
+	
+	
+	
+	
+
+	public boolean isFiltroP() {
+		return filtroP;
+	}
+
+	public void setFiltroP(boolean filtroP) {
+		this.filtroP = filtroP;
+	}
+
+	public boolean isFiltroF() {
+		return filtroF;
+	}
+
+	public void setFiltroF(boolean filtroF) {
+		this.filtroF = filtroF;
+	}
+
+	public List<Curriculo> getCl() {
+		return cl;
+	}
+
+	public void setCl(List<Curriculo> cl) {
+		this.cl = cl;
+	}
+
+	public Filtro getFiltroSelecionado() {
+		return filtroSelecionado;
+	}
+
+	public void setFiltroSelecionado(Filtro filtroSelecionado) {
+		this.filtroSelecionado = filtroSelecionado;
+	}
+
+	public Curriculo getCur() {
+		return cur;
+	}
+
+	public void setCur(Curriculo cur) {
+		this.cur = cur;
+	}
+
+	public Formacao getFf() {
+		return ff;
+	}
+
+	public void setFf(Formacao ff) {
+		this.ff = ff;
+	}
+
+	public Experiencia getXp() {
+		return xp;
+	}
+
+	public void setXp(Experiencia xp) {
+		this.xp = xp;
+	}
+
+	public CurriculoDAO getClDAO() {
+		return clDAO;
+	}
+
+	public void setClDAO(CurriculoDAO clDAO) {
+		this.clDAO = clDAO;
+	}
+
+}
